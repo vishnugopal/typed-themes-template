3d57a1ed0c396aa4344127c2606055247ccbf9bb
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2020-05-03 11:09:44 +0530

    Delete all code

--- src/tests/components/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Button from '../../components/Button';
-import lightTheme from '../../themes/lightTheme';
-
-test('renders a button', () => {
-    const { getByRole } = render(<Button theme={lightTheme} title="Hello" />);
-    expect(getByRole('button')).toHaveTextContent('Hello');
-});
